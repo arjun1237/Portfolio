dfdb1cee3c5ce22eff46cdcf4e04bd24b16b750e
Arjun K Prasad <arjun1237.kp@gmail.com>
2021-05-03 05:08:46 +0530

    web designing done

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
+import styles from './styles/Contact.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 export const Contact = () => {
     return (
         <section id="contact">
-            
+            <div className={styles.contactWrap}>
+                <h1>Hire me <span>NOW</span>, before someone else does!</h1>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Name:</td>
+                            <td>Arjun K Prasad</td>
+                        </tr>
+                        <tr>
+                            <td>Email:</td>
+                            <td>arjun1237.kp@gmail.com</td>
+                        </tr>
+                        <tr>
+                            <td>Phone:</td>
+                            <td>+91-7349622070</td>
+                        </tr>
+                        <tr>
+                            <td>Social Media Reach:</td>
+                            <td className={styles.sMedia}>
+                                <a href="https://github.com/arjun1237" title="GitHub link" target="_blank" rel="noopener noreferrer">
+                                    <FontAwesomeIcon icon={faGithub} />
+                                </a>
+                                <a href="https://www.linkedin.com/in/arjun1237/" title="LinkedIn link" target="_blank" rel="noopener noreferrer">
+                                    <FontAwesomeIcon icon={faLinkedinIn} />
+                                </a>
+                                <a href="https://twitter.com/arjun1237_kp" title="Twitter Link" target="_blank" rel="noopener noreferrer">
+                                    <FontAwesomeIcon icon={faTwitter} />
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Current Location:</td>
+                            <td>Bengaluru</td>
+                        </tr>
+                        <tr>
+                            <td>Willing to relocate?:</td>
+                            <td>Yes, after pandemic ends.</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </section>
     )
 }
--- src/Components/StacksStats.jsx
+++ src/Components/StacksStats.jsx
@@ -5,18 +5,24 @@ export const StacksStats = () => {
     return (
         <section id="stack">
             <div className={styles.stackwrap}>
-                <div className={styles.one}>
-                    <div className={styles.stackImg}>
-                        <h1>Tech Stack &#38; Stats</h1>
-                        <img src={process.env.PUBLIC_URL + '/resources/undraw_developer_activity_bv83.svg'} alt=""/>
-                    </div>
-                </div>
-                <div className={styles.two}>
+                <h1>TECH STACKS &#38; GITHUB STATS</h1>
+                <div className={styles.stacks}>
                     {stacks.map(stack => 
-                        <img src={stack} alt=""/>
+                        <img src={stack} alt="" key={stack}/>
                     )}
                 </div>
-                <div className={styles.three}>
+                <div className={styles.stats}>
+                    <div className={styles.images}>
+                        <div className={styles.statImg}>
+                            <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=arjun1237&theme=radical" alt=""/>
+                        </div>
+                        <div className={styles.githubStats}>
+                            <img src="https://github-readme-stats.vercel.app/api?username=arjun1237&show_icons=true&theme=radical&include_all_commits=true" alt=""/>
+                        </div>
+                        <div className={styles.stackImg}>
+                            <img src={process.env.PUBLIC_URL + '/resources/undraw_developer_activity_bv83.svg'} alt=""/>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
@@ -25,12 +31,12 @@ export const StacksStats = () => {
 
 
 const stacks = [
-    process.env.PUBLIC_URL + '/resources/html-5.svg',
-    process.env.PUBLIC_URL + '/resources/css-3.svg',
     process.env.PUBLIC_URL + '/resources/javascript.svg',
+    process.env.PUBLIC_URL + '/resources/html-5.svg',
     process.env.PUBLIC_URL + '/resources/material-ui.svg',
+    process.env.PUBLIC_URL + '/resources/css-3.svg',
+    process.env.PUBLIC_URL + '/resources/bootstrap.svg',
     process.env.PUBLIC_URL + '/resources/react.svg',
     process.env.PUBLIC_URL + '/resources/redux.svg',
     process.env.PUBLIC_URL + '/resources/nodejs-icon.svg',
-    process.env.PUBLIC_URL + '/resources/bootstrap.svg'
 ]
